4b63e772af8c746740f75fb154a88654fa1568a8
Weuller Oliveira <weullerwww@gmail.com>
2022-01-11 10:36:16 -0300

    Instanciando OrderSatus com uma String

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,12 @@ public class Program {
 		Order order = new Order(1086, new Date(), OrderStatus.PENDING_PAIMENT);
 		
 		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 
 	}
 
